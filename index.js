d70080afbdad202d69aeb5c5951e340154fa66c4
danegreice <114593443+danegreice@users.noreply.github.com>
2024-05-16 16:30:42 +0000

    read,  insert e update com mongodb

--- index.js
+++ index.js
@@ -1,55 +1,78 @@
 const express = require("express");
-const app = express();
+const { MongoClient, ObjectId } = require("mongodb");
 
-app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello world, Dani");
-})
+//const dbUrl = "mongodb+srv://danielegreice:IVems79OpxSluQcC@cluster0.4zfth2g.mongodb.net"
+//const dbName = "jornada-backend"
 
-app.get("/oi", (req, res) => {
-    res.send("Olá mundo");
-})
+const dbUrl = "mongodb+srv://admin:JHy9QG6y9kLJItWK@cluster0.jg2n1i9.mongodb.net"
+const dbName = "ocean-jornada-backend-maio-2024"
 
-const itens = ["Elena Gilbert", "Stefan Salvatore", "Damon Salvatore"];
+const client = new MongoClient(dbUrl);
+const main = async () => {
+    console.log("Conectando com o banco de dados...");
+    await client.connect();
+    console.log("Banco de dados conectado com suceso");
 
-app.get("/item", (req, res) => {
-    res.status(200).json(itens.filter(Boolean));
-})
+    const app = express();
 
-app.get("/item/:id", (req, res) => {
-    const id = req.params.id;
+    app.use(express.json())
 
-    const item = itens[id-1];
-    res.status(200).json(item);
-})
+    app.get("/", (req, res) => {
+        res.send("Hello world, Dani");
+    })
 
-app.post("/item", (req, res) => {
-    const body = req.body;
-    const novoItem = body.nome;
+    app.get("/oi", (req, res) => {
+        res.send("Olá mundo");
+    })
 
-    itens.push(novoItem);
-    
-    res.status(200).json();
-})
+    const itens = ["Elena Gilbert", "Stefan Salvatore", "Damon Salvatore"];
 
-app.put("/item/:id", (req, res) => {
-    const id = req.params.id;
+    const db = client.db(dbName);
+    const collection = db.collection("item");
 
-    const body = req.body;
-    const itemAtualizado = body.nome;
+    app.get("/item", async (req, res) => {
+        const documentos = await collection.find().toArray();
+        res.status(200).json(documentos);
+    })
 
-    itens[id-1] = itemAtualizado;
-    
-    res.status(200).json();
-})
+    app.get("/item/:id", async (req, res) => {
+        const id = req.params.id;
 
-app.delete("/item/:id", (req, res) => {
-    const id = req.params.id;
+        const item = await collection.findOne({_id: new ObjectId(id)});
+        res.status(200).json(item);
+    })
 
-    delete itens[id-1];
-    
-    res.status(200).json();
-})
+    app.post("/item", (req, res) => {
+        const body = req.body;
+        const novoItem = body.nome;
 
-app.listen(3000);
+        collection.insertOne({nome: novoItem})
+        //itens.push(novoItem);
+
+        res.status(200).json();
+    })
+
+    app.put("/item/:id", async (req, res) => {
+        const id = req.params.id;
+
+        const body = req.body;
+        const itemAtualizado = body.nome;
+
+        await collection.updateOne({_id: new ObjectId(id)}, {$set: {nome: itemAtualizado}})
+
+        res.status(200).json();
+    })
+
+    app.delete("/item/:id", (req, res) => {
+        const id = req.params.id;
+
+        delete itens[id - 1];
+
+        res.status(200).json();
+    })
+
+    app.listen(3000);
+}
+
+main();
